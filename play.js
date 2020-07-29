f0bace73a2370e45323c891c8234f63158469536
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-07-29 17:23:45 +0000

    Play.js now requires client.js, which establishes a connection

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require('net');
-
-//This will establish connection to the game server
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  //Handles message sent to client from server 
-    //In this case, when you idle, it tells you, you died
-  conn.on('data', (data) => {
-    console.log('You died because you idled');
-  });
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
